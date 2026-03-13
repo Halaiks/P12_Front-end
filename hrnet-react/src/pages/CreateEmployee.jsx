872fc23850a0f054d67b08554541e698d3e526ca
Alexandre GUILLAUME <AlexGuillaume94@gmail.com>
2026-03-13 09:56:12 +0100

    Sauvegarde employé dans LocalStorage

--- hrnet-react/src/pages/CreateEmployee.jsx
+++ hrnet-react/src/pages/CreateEmployee.jsx
@@ -1,5 +1,117 @@
+import { useState } from "react"
+
 function CreateEmployee() {
-  return <h2>Create Employee</h2>
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    startDate: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    department: ""
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const employees = JSON.parse(localStorage.getItem("employees")) || []
+
+  employees.push(formData)
+
+  localStorage.setItem("employees", JSON.stringify(employees))
+
+  console.log("Employee saved :", formData)
+  }
+
+  return (
+    <div>
+      <h2>Create Employee</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        <label>First Name</label>
+        <input
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+
+        <label>Last Name</label>
+        <input
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+
+        <label>Date of Birth</label>
+        <input
+          type="date"
+          name="dateOfBirth"
+          value={formData.dateOfBirth}
+          onChange={handleChange}
+        />
+
+        <label>Start Date</label>
+        <input
+          type="date"
+          name="startDate"
+          value={formData.startDate}
+          onChange={handleChange}
+        />
+
+        <label>Street</label>
+        <input
+          name="street"
+          value={formData.street}
+          onChange={handleChange}
+        />
+
+        <label>City</label>
+        <input
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
+        />
+
+        <label>State</label>
+        <input
+          name="state"
+          value={formData.state}
+          onChange={handleChange}
+        />
+
+        <label>Zip Code</label>
+        <input
+          name="zipCode"
+          value={formData.zipCode}
+          onChange={handleChange}
+        />
+
+        <label>Department</label>
+        <input
+          name="department"
+          value={formData.department}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Save</button>
+
+      </form>
+
+    </div>
+  )
 }
 
 export default CreateEmployee
